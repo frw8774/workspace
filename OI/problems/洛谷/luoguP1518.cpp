6b15981c8f7dd3a4dd673057cfe579bfedfdb5f3
frw8774 <3964438900@qq.com>
2026-01-22 00:10:24 +0800

    还没写完，继续写

---
+++ OI/problems/洛谷/luoguP1518.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void input() {
+    char map[10][10];
+    for(int i = 0;i < 10;i++) {
+        for(int j = 0;j < 10;j++) {
+            cin>>map[i][j];
+        }
+    }
+}
+int main() {
+    return 0;
+}
