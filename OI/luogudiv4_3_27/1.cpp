6eaa01533b38d7342388962b22049ab683a85af0
frw8774 <3964438900@qq.com>
2026-03-27 20:50:21 +0800

    新增题解

---
+++ OI/luogudiv4_3_27/1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve() {
+    int a, b, c, d;
+    int n;
+    cin >> n >> a >> b >> c >> d;
+    cout << n - a - b - c - d << endl;
+}
+int main() {
+    solve();
+    return 0;
+}
