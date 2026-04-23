8b940a99a78589d98a4638b8de45ca3d6bd9c350
frw8774 <3964438900@qq.com>
2026-04-23 20:56:15 +0800

    新增题解

---
+++ OI/problems/codeforces/2218A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve() {
+    int x;
+    cin >> x;
+    cout << x << endl;
+}
+int main() {
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++) {
+        solve();
+    }
+    return 0;
+}
